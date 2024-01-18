9cd1aa12e7b8f6838fefce91fbd60023555d1613
inlancersystem4 <inlancersystem4@gmail.com>
2024-01-18 11:42:24 +0530

    add code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-document.getElementById('openSidebar').addEventListener('click', function () {
-    document.querySelector('.layout-sidebar').classList.toggle('responsive_sidebar');
-});
 
-document.getElementById('closeSidebar').addEventListener('click', function () {
-    document.querySelector('.layout-sidebar').classList.remove('responsive_sidebar');
-});
+
